d0f9c855448d67a76102b721ed6f116c43c9a819
Nauris Pūķis <hi@pyronaur.com>
2022-10-01 23:31:13 +0300

    Add a couple post-component templates

---
+++ src/components/post-components/BookQuote.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let book: string;
+	export let author: string;
+</script>
+
+<div class="book-quote">
+	<h1>{book}</h1>
+	<p>by {author}</p>
+	<slot />
+</div>
---
+++ src/components/post-components/FailedAttempts.svelte
@@ -0,0 +1,11 @@
+
+
+<div class="failed-attempts">
+	<slot />
+</div>
+
+<style lang="scss">
+	.failed-attempts {
+		color: red;
+	}
+</style>
