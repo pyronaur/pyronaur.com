75ec329603be21e5a70c380edfe476070e77729e
Nauris Pūķis <hi@pyronaur.com>
2022-10-04 20:51:46 +0300

    Clean up color scheme toggle

---
+++ src/components/blog/SiteThemeToggle.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+	function isDarkMode() {
+		if (import.meta.env.SSR) {
+			return false;
+		}
+
+		const sessionData = window.sessionStorage.getItem("darkMode");
+		if (sessionData) {
+			return "true" === sessionData;
+		}
+
+		return window.matchMedia("(prefers-color-scheme: dark)").matches;
+	}
+
+	function toggleWithKeyboard(e: KeyboardEventInit) {
+		if (e.key !== "Escape") {
+			return;
+		}
+		darkMode = !darkMode;
+	}
+
+	function triggerSpin(duration: number) {
+		spin = true;
+		setTimeout(() => (spin = false), duration);
+	}
+
+	function toggleColorScheme(enableDarkMode = false) {
+		if (import.meta.env.SSR) {
+			return false;
+		}
+
+		// Compare the current status to avoid triggering the animation on load.
+		const bodyClasses = Array.from(document.body.classList);
+		const domLightMode = bodyClasses.includes("light");
+		const domDarkMode = bodyClasses.includes("dark");
+
+		// If the DOM already has the correct class, don't do anything.
+		// But only if DOM has a class, otherwise this is the first load
+		// and we need to set the correct class.
+		if ((domDarkMode || domLightMode) && domDarkMode === enableDarkMode) {
+			return;
+		}
+
+		// Change the body class based on the current status.
+		if (darkMode) {
+			spinDirection = "left";
+			document.body.classList.remove("light");
+			document.body.classList.add("dark");
+		} else {
+			spinDirection = "right";
+			document.body.classList.remove("dark");
+			document.body.classList.add("light");
+		}
+
+		// Trigger the animation
+		triggerSpin(650);
+
+		// Save the dark mode status in the session storage.
+		sessionStorage.setItem("darkMode", darkMode.toString());
+	}
+
+	let spinDirection: "left" | "right" = "right";
+	let darkMode = isDarkMode();
+	let spin = false;
+	$: toggleColorScheme(darkMode);
+</script>
+
+<svelte:window on:keydown={toggleWithKeyboard} />
+
+<button id="site-theme-toggle" on:click={() => (darkMode = !darkMode)}>
+	<div
+		class="icon"
+		class:spin-left={spin && spinDirection === "left"}
+		class:spin-right={spin && spinDirection === "right"}
+	>
+		<img src="/assets/moon.svg" alt="Enable Light Mode" class="icon-moon" />
+		<img src="/assets/sun.svg" alt="Enable Dark Mode" class="icon-sun" />
+	</div>
+</button>
+
+<style lang="scss">
+	#site-theme-toggle {
+		background: none;
+		border: none;
+		cursor: pointer;
+		padding: 0;
+		outline: 0;
+	}
+
+	.spin {
+		animation: spin 575ms cubic-bezier(0.075, 0.82, 0.17, 1.135);
+	}
+
+	.spin-right {
+		@extend .spin;
+		@keyframes spin {
+			0% {
+				transform: scale(0) rotate(0deg);
+			}
+			100% {
+				transform: scale(1) rotate(720deg);
+			}
+		}
+	}
+	.spin-left {
+		@extend .spin;
+		@keyframes spin {
+			0% {
+				transform: scale(0) rotate(0deg);
+			}
+			100% {
+				transform: scale(1) rotate(-720deg);
+			}
+		}
+	}
+	img {
+		display: none;
+	}
+	:global(.dark) .icon-moon {
+		display: block;
+	}
+
+	:global(.light) .icon-sun {
+		display: block;
+	}
+</style>
