d553b5533a5d8f830bee4837bd4db909cee63c1c
Nauris Pūķis <hi@pyronaur.com>
2022-10-09 15:40:40 +0300

    Make posts strongly typed

--- src/data/post.ts
+++ src/data/post.ts
@@ -1,20 +1,30 @@
+import { type MarkdownInstance } from "astro";
+
 // Which mode is the environment running in? https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
 const { MODE } = import.meta.env;
 
-export type Post = {
+export type PostMarkdownInstance = MarkdownInstance<{
+	title: string;
+	date: string;
+	preview: string;
+}>;
+
+export interface Post {
 	title: string,
 	slug: string,
 	preview: string,
 	timestamp: number,
 	draft: boolean,
 	date: string,
-	file: string,
+	file: string;
+	Content: PostMarkdownInstance["Content"];
 }
 
-export function single(post): Post {
+export function post(post: PostMarkdownInstance): Post {
 
 	const file = post.file;
-	const slug = file.split('/').reverse()[0].replace('.mdx', '').replace('.md', '');
+	const filename = file.split('/').pop();
+	const slug = filename.replace(/\.mdx?/, '')
 	const contentPath = file.split("content/")[1];
 	const draft = contentPath.split('/')[0] !== 'public';
 
@@ -28,15 +38,19 @@ export function single(post): Post {
 	}
 }
 
-export function blog(posts): Post[] {
-	return published(posts)
+export function posts(paths: PostMarkdownInstance[]): Post[] {
+	return paths
+		.filter(post => post.frontmatter.title)
+		.map(post)
+}
+
+export function blog(paths: PostMarkdownInstance[]): Post[] {
+	return published(paths)
 		.filter(post => !post.file.includes("/archive/"));
 }
 
-export function published(posts): Post[] {
-	return posts
-		.filter(post => post.frontmatter.title)
-		.map(single)
+export function published(paths: PostMarkdownInstance[]): Post[] {
+	return posts(paths)
 		.filter(post => MODE === 'development' || !post.draft)
 		.sort((a, b) => b.timestamp - a.timestamp)
 }
