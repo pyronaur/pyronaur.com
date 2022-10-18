9353841fdaff297aa78a4bbbca5e24c99f34d138
Nauris Pūķis <hi@pyronaur.com>
2022-10-18 23:54:25 +0300

    Ensure the target is static

--- astro.config.ts
+++ astro.config.ts
@@ -5,21 +5,22 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    remarkPlugins: ['remark-gfm'],
-    rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
-      behavior: 'wrap'
-    }]],
-    // Pick a syntax highlighter. Can be 'prism' (default), 'shiki' or false to disable any highlighting.
-    syntaxHighlight: 'shiki',
-    // If you are using shiki, here you can define a global theme and
-    // add custom languages.
-    shikiConfig: {
-      theme: 'nord',
-      langs: [],
-      wrap: false
-    }
-  },
-  site: "https://pyronaur.com",
-  integrations: [ mdx(), sitemap(),  svelte()]
+	markdown: {
+		remarkPlugins: ['remark-gfm'],
+		rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+			behavior: 'wrap'
+		}]],
+		// Pick a syntax highlighter. Can be 'prism' (default), 'shiki' or false to disable any highlighting.
+		syntaxHighlight: 'shiki',
+		// If you are using shiki, here you can define a global theme and
+		// add custom languages.
+		shikiConfig: {
+			theme: 'nord',
+			langs: [],
+			wrap: false
+		}
+	},
+	site: "https://pyronaur.com",
+	integrations: [mdx(), sitemap(), svelte()],
+	output: 'static',
 });
