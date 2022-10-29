2b9b46ca6fbc987ed53e4304275978e6fabec631
Nauris Pūķis <hi@pyronaur.com>
2022-10-29 23:12:46 +0300

    Clean up astro config

--- astro.config.ts
+++ astro.config.ts
@@ -7,10 +7,6 @@ import serviceWorker from "astrojs-service-worker";
 // https://astro.build/config
 export default defineConfig({
 	markdown: {
-		remarkPlugins: ['remark-gfm'],
-		rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
-			behavior: 'wrap'
-		}]],
 		// Pick a syntax highlighter. Can be 'prism' (default), 'shiki' or false to disable any highlighting.
 		syntaxHighlight: 'shiki',
 		// If you are using shiki, here you can define a global theme and
@@ -22,6 +18,10 @@ export default defineConfig({
 		}
 	},
 	site: "https://pyronaur.com",
-	integrations: [mdx(), sitemap(), svelte(), serviceWorker()],
+	integrations: [serviceWorker({
+		workbox: {
+			globPatterns: [`**/*.{js,css,html,svg,png,jpg,jpeg,webp,woff,woff2,ttf,otf,ico,webmanifest,xml}`],
+		}
+	}),mdx(), sitemap(), svelte(),],
 	output: 'static',
 });
