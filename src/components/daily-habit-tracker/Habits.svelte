fdd52fa7c9cdd1cc88f934b5a9eec134ddc8d47b
Nauris Pūķis <hi@pyronaur.com>
2022-12-16 23:08:36 +0200

    Refactor habit tracker, WIP

--- src/components/daily-habit-tracker/Habits.svelte
+++ src/components/daily-habit-tracker/Habits.svelte
@@ -1,134 +1,116 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-    import { fetchHabit } from "./api";
-	import Entry from "./Entry.svelte";
+	import { onMount } from 'svelte';
+  import { toggleHabit, getDays, Habit, habits as targets } from "./api";
+	import { getCredentials } from './auth';
+
+  export let habits: Habit[];
+  const dailyHabits = getDays(habits);
+  let filter = '';
+  $: days = filter
+  	? dailyHabits.map( day => {
+		return {
+			...day,
+			slugs: day.slugs.filter( slug => slug === filter)
+		}
+	})
+	: dailyHabits;
 	
-	export let history: number[];
-	export let name: string;
+	let isLoggedIn = false;
 
-	type JournalEntry = {
-		day: number;
-		complete: boolean;
-	};
-
-	
-	function dateToDays(date: Date) {
-		return Math.floor(date.getTime() / 86400000);
-	}
-
-	function entriesFromHistory( history ): JournalEntry[] {
-		const minDays = 30;
-		const today = dateToDays(new Date());
-		let start = history[history.length - 1] || today - minDays;
-		if (start > today - minDays) {
-			start = today - minDays;
-		}
-
-		let entries = [];
-		for (let day = start; day <= today; day++) {
-			entries.unshift({
-				day,
-				complete: history.includes(day),
-			});
-		}
-
-		return entries;
-	}
-	
-
-	function getCookie(key) {
-		const cookies = document.cookie.split(";");
-		const cookie = cookies.find((c) => c.includes(key));
-		return cookie ? cookie.split("=")[1] : null;
-	}
-
-	let loggedIn = false;
-	let isLoading = false;
-	let username;
-	let token;
-	onMount(async () => {
-		username = getCookie("username");
-		token = getCookie("token");
-		if (username && token.length >= 32) {
-			loggedIn = true;
-		}
-		if ( entries && !entries[0].complete) {
-			const freshHistory = await fetchHabit(name);
-			history = freshHistory;
-		}
-	});
-
-	async function submit(e) {
-		
-		if (!loggedIn) {
-			console.error("Not logged in");
-			return;
-		}
-
-		isLoading = true;
-
-		const targetDay = parseInt(e.srcElement.dataset.day);
-
-		if (!targetDay) {
-			return;
-		}
-
-		const entryIndex = entries.findIndex(
-			(entry) => entry.day === targetDay
-		);
-
-		const entry = entries[entryIndex];
-		if (!entry) {
-			return;
-		}
-		entry.complete = !entry.complete;
-		const updatedHistory = entries
-			.filter((entry) => entry.complete)
-			.map((entry) => entry.day);
-		const response = await fetch(`https://api.pyronaur.com/habits/${name}`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				"X-PY-USER": username,
-				"X-PY-KEY": token,
-			},
-			body: JSON.stringify(updatedHistory)
-		});
-		if (response.status === 200) {
-			history = updatedHistory;
-		} else {
-			// Revert the value on failure
-			entry.complete = !entry.complete;
-		}
-		isLoading = false;
-	}
-
-	$: entries = entriesFromHistory(history);
-
+	onMount( () => {
+		isLoggedIn = getCredentials() !== false
+	})
 </script>
-
-<div class="track" class:loading={isLoading}>
-	{#each entries as entry, i}
-		<Entry
-			day={entry.day}
-			complete={entry.complete}
-			actionable={loggedIn}
-			on:click={submit}
-		/>
-	{/each}
+{#each habits as habit}
+	<button
+	class="habit-group"
+	class:actionable={isLoggedIn}
+	href="#{habit.slug}"
+	on:click={ async () => habit = await toggleHabit(habit)}
+	on:mouseleave={() => filter = ''}
+	on:mouseenter={() => filter = habit.slug}>
+		{habit.label}
+</button>
+{/each}
+<div class="habits" class:filter style:--opacity-step={1/(targets.length-1)}>
+{#each days as day}
+	<div class="day">
+		{#each day.slugs as habit}
+			<i class="block habit-{habit}"></i>
+		{/each}
+	</div>
+{/each}
 </div>
 
 <style lang="scss">
-	.track {
-		$size: 36px;
+	$size: 24px;
+
+
+	.actionable {
+		cursor: pointer;
+	}
+
+	.habits {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax($size, 1fr));
 		grid-auto-rows: $size;
-		gap: 10px;
+		gap: 6px;
 
 		&.loading {
 			opacity: 0.5;
 			pointer-events: none;
+		}
+	}
+
+	button {
+		margin-top: 50px;
+		margin-bottom: 10px;
+		display: inline-block;
+		padding: 7px 20px;
+		background-color: var(--habit-button);
+		color: white;
+		text-decoration: none;
+		transition: background-color 0.2s ease-in-out;
+		border: none;
+		font-size: .8em;
+		
+		&:hover {
+			background-color: var(--habit-full);
+		}
+
+		&:first-of-type {
+			border-top-left-radius: 4px;
+			border-bottom-left-radius: 4px;;
+		}
+		&:last-of-type {
+			border-top-right-radius: 4px;
+			border-bottom-right-radius: 4px;
+		}
+	}
+
+	.day {
+		position: relative;
+		background-color: var(--habit);
+		border-radius: 4px;
+		overflow: hidden;
+	}
+
+	.block {
+		display: block;
+		position: absolute;
+		left: 0;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		z-index: 150;
+
+		background-color: var(--habit-done);
+		opacity: calc( var(--opacity-step) * 2 );
+		mix-blend-mode: var(--habit-blend-mode);
+		.filter & {
+			opacity: 1;
+			background-color: var(--habit-full);
+			mix-blend-mode: normal;
 		}
 	}
 </style>
--- src/components/daily-habit-tracker/api.ts
+++ src/components/daily-habit-tracker/api.ts
@@ -1,7 +1,122 @@
+import { getCookie, getCredentials, isLoggedIn } from "./auth";
+
+export type Habit = {
+	label: string;
+	slug: string;
+	history: number[];
+};
+
+export const habits: Habit[] = [
+	{
+		label: "Morning Walk",
+		slug: "walk",
+		history: [],
+	},
+	{
+		label: "Code",
+		slug: "code",
+		history: [],
+	},
+	{
+		label: "Writing",
+		slug: "write",
+		history: [],
+	},
+	{
+		label: "Track",
+		slug: "track",
+		history: [],
+	},
+];
+
+export type Days = Array<{
+	day: number;
+	slugs: string[];
+}>;
+
+function dateToDays(date: Date) {
+	return Math.floor(date.getTime() / 86400000);
+}
+
+export function getDays(habits: Habit[]): Days {
+	// First, find the earliest day that any habit was tracked
+	let earliestDate = new Date();
+	earliestDate.setDate(earliestDate.getDate() - 90);
+	let earliestDay = dateToDays(earliestDate);
+
+	habits.forEach((habit) => {
+		const minHistory = Math.min(...habit.history);
+		if (minHistory < earliestDay) {
+			earliestDay = minHistory;
+		}
+	});
+
+	// Next, create a map of days to the habits that were tracked on that day
+	const daysToHabits = new Map<number, string[]>();
+	habits.forEach((habit) => {
+		habit.history.forEach((day) => {
+			if (!daysToHabits.has(day)) {
+				daysToHabits.set(day, []);
+			}
+			daysToHabits.get(day)!.push(habit.slug);
+		});
+	});
+
+	// Finally, create the array of Days objects, including entries for every day
+	// from the earliest day to today
+	const days: Days = [];
+	const today = dateToDays(new Date());
+	for (let day = earliestDay; day <= today; day++) {
+		days.push({
+			day,
+			slugs: daysToHabits.get(day) || [],
+		});
+	}
+	return days.sort((a, b) => b.day - a.day);
+}
 
 export async function fetchHabit(name): Promise<number[]> {
 	const response = await fetch(`https://api.pyronaur.com/habits/${name}`);
 	const data = await response.json();
 
 	return data;
+}
+
+export async function updateHabit(name: string, history: number[]) {
+	const credentials = getCredentials();
+	if (!credentials) {
+		console.error("Not logged in");
+		return;
+	}
+	const { username, token } = credentials;
+	const response = await fetch(`https://api.pyronaur.com/habits/${name}`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"X-PY-USER": username,
+			"X-PY-KEY": token,
+		},
+		body: JSON.stringify(history),
+	});
+	if (response.status !== 200) {
+		console.error(response);
+	}
+	return response.status === 200;
+}
+
+export async function toggleHabit(habit: Habit) {
+	const today = dateToDays(new Date());
+	let history = [];
+	if (habit.history[0] === today) {
+		history = habit.history.slice(1);
+	} else {
+		history = [today, ...habit.history];
+	}
+
+	const success = await updateHabit(habit.slug, history);
+	if (success) {
+		habit.history = history;
+		return habit;
+	}
+	return habit;
 }
