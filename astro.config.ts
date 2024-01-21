7dbdd9949acb89e150ca38df5665e9ddc5601855
Nauris Pūķis <hi@pyronaur.com>
2024-01-21 10:44:59 +0200

    Upgrade now compiles

--- astro.config.ts
+++ astro.config.ts
@@ -6,28 +6,31 @@ import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		remarkPlugins: [remarkGfm],
-		rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
-		// Pick a syntax highlighter. Can be 'prism' (default), 'shiki' or false to disable any highlighting.
-		syntaxHighlight: 'shiki',
-		// If you are using shiki, here you can define a global theme and
-		// add custom languages.
-		shikiConfig: {
-			theme: 'nord',
-			langs: [],
-			wrap: false
-		}
-	},
-	site: "https://pyronaur.com",
-	integrations: [mdx(), sitemap(), svelte()],
-	output: 'static',
-	trailingSlash: 'never',
-	build: {
-		inlineStylesheets: 'auto',
-	},
-	compressHTML: true,
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: 'wrap'
+    }]],
+    // Pick a syntax highlighter. Can be 'prism' (default), 'shiki' or false to disable any highlighting.
+    syntaxHighlight: 'shiki',
+    // If you are using shiki, here you can define a global theme and
+    // add custom languages.
+    shikiConfig: {
+      theme: 'nord',
+      langs: [],
+      wrap: false
+    }
+  },
+  site: "https://pyronaur.com",
+  integrations: [mdx(), sitemap(), svelte(), icon()],
+  output: 'static',
+  trailingSlash: 'never',
+  build: {
+    inlineStylesheets: 'auto'
+  },
+  compressHTML: true
 });
